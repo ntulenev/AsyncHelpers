ac225978b26edb19c0a57296a9687166c6e201e6
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-09 20:30:51 +0300

    Add RechargeableCompletionSource

---
+++ src/AsyncHelpers/AsyncHelpers/RechargeableCompletionSource.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+
+    public class RechargeableCompletionSource<T>
+    {
+        public RechargeableCompletionSource(bool runContinuationsAsynchronously)
+        {
+            _runContinuationsAsynchronously = runContinuationsAsynchronously;
+            CreateCompletionSource();
+        }
+
+        private void CreateCompletionSource()
+        {
+            if (_runContinuationsAsynchronously)
+                _tcs = new TaskCompletionSource<T>(TaskCreationOptions.RunContinuationsAsynchronously);
+            else
+                _tcs = new TaskCompletionSource<T>();
+        }
+
+        public void SetResultAndWait(T data)
+        {
+            lock (_setResultGuard)
+            {
+                _tcs.SetResult(data);
+                _are.WaitOne();
+            }
+        }
+
+        public async Task<ResultContainer<T>> GetValueAsync()
+        {
+            lock (_getValueAsyncGuard)
+            {
+                if (_isValueIsWork)
+                {
+                    throw new InvalidOperationException("ResultContainer was already got but not disposed yet");
+                }
+                _isValueIsWork = true;
+            }
+
+            await _tcs.Task.ConfigureAwait(false);
+
+            return new ResultContainer<T>(() =>
+            {
+                CreateCompletionSource();
+                _isValueIsWork = false;
+                _are.Set();
+
+            }, _tcs.Task.Result);
+        }
+
+        private TaskCompletionSource<T> _tcs = default!;
+
+        private volatile bool _isValueIsWork = false;
+
+        private readonly bool _runContinuationsAsynchronously;
+
+        private readonly AutoResetEvent _are = new AutoResetEvent(false);
+
+        private readonly object _setResultGuard = new object();
+        private readonly object _getValueAsyncGuard = new object();
+
+
+    }
+}
---
+++ src/AsyncHelpers/AsyncHelpers/ResultContainer{T}.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace AsyncHelpers
+{
+    public class ResultContainer<T> : IDisposable
+    {
+        public ResultContainer(Action resetAction, T value)
+        {
+            if (resetAction is null)
+                throw new ArgumentNullException(nameof(resetAction));
+
+            _resetAction = resetAction;
+            Value = value;
+        }
+
+        public T Value { get; }
+
+        private readonly Action _resetAction;
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                _resetAction();
+                _isDisposed = true;
+            }
+        }
+
+        private bool _isDisposed;
+    }
+}
