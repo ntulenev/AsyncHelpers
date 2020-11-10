a72e8932919845361b851e75fe8489d9405d6295
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-10 18:25:07 +0300

    Fix namespaces

--- src/AsyncHelpers/AsyncHelpers.Tests/RechargeableCompletionSource{T}.Tests.cs
+++ src/AsyncHelpers/AsyncHelpers.Tests/RechargeableCompletionSource{T}.Tests.cs
@@ -1,8 +1,7 @@
-﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+
+using FluentAssertions;
+
 using Xunit;
 
 namespace AsyncHelpers.Tests
