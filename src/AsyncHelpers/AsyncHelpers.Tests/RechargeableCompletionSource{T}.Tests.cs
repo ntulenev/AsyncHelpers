925d4025c27c981add5c90dbb4ad3cded01eb9f6
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-10 18:24:45 +0300

    Add RCS Tests

--- src/AsyncHelpers/AsyncHelpers.Tests/RechargeableCompletionSource{T}.Tests.cs
+++ src/AsyncHelpers/AsyncHelpers.Tests/RechargeableCompletionSource{T}.Tests.cs
@@ -1,10 +1,101 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace AsyncHelpers.Tests
 {
-    class RechargeableCompletionSourceTests
+    public class RechargeableCompletionSourceTests
     {
+        [Theory(DisplayName = "The RechargeableCompletionSource can be constructed")]
+        [InlineData(true)]
+        [InlineData(false)]
+        [Trait("Category", "Unit")]
+        public void CanBeConstructedActionAndAnyValue(bool runContinuationsAsynchronously)
+        {
+            // Act
+            var exception = Record.Exception(
+                () => new RechargeableCompletionSource<object>(runContinuationsAsynchronously));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Theory(DisplayName = "The RechargeableCompletionSource throws exception if run GetValueAsync twice")]
+        [InlineData(true)]
+        [InlineData(false)]
+        [Trait("Category", "Unit")]
+        public void RechargeableCompletionSourceThrowsErrorOnGetValueAsync(bool runContinuationsAsynchronously)
+        {
+            // Arrange
+            var rcs = new RechargeableCompletionSource<object>(runContinuationsAsynchronously);
+
+            // Act
+            var t1 = rcs.GetValueAsync();
+            var t2 = rcs.GetValueAsync();
+
+            // Assert
+            t1.IsFaulted.Should().BeFalse();
+            t2.IsFaulted.Should().BeTrue();
+        }
+
+        [Theory(DisplayName = "The RechargeableCompletionSource blocks set until get result and dispose (case with await after)")]
+        [InlineData(true)]
+        [InlineData(false)]
+        [Trait("Category", "Unit")]
+        public async Task RechargeableCompletionSourceBlocksSetAwaitAfter(bool runContinuationsAsynchronously)
+        {
+            // Arrange
+            var rcs = new RechargeableCompletionSource<object>(runContinuationsAsynchronously);
+
+            // Act
+            var t = Task.Run(() => rcs.SetResultAndWait(null!));
+
+            await Task.Delay(1_000); // Attempts to ensure that task t never completes.
+
+            // Assert
+            t.IsCompleted.Should().BeFalse();
+
+            var result = await rcs.GetValueAsync();
+
+            t.IsCompleted.Should().BeFalse();
+
+            result.Dispose();
+
+            await Task.Delay(1_000); // Attempts to ensure that t will change status after work will finished.
+
+            t.IsCompleted.Should().BeTrue();
+        }
+
+        [Theory(DisplayName = "The RechargeableCompletionSource blocks set until get result and dispose (case with await after)")]
+        [InlineData(true)]
+        [InlineData(false)]
+        [Trait("Category", "Unit")]
+        public async Task RechargeableCompletionSourceBlocksSetAwaitBefore(bool runContinuationsAsynchronously)
+        {
+            // Arrange
+            var rcs = new RechargeableCompletionSource<object>(runContinuationsAsynchronously);
+
+            // Act
+            var result = rcs.GetValueAsync();
+
+            var t = Task.Run(() => rcs.SetResultAndWait(null!));
+
+            await Task.Delay(1_000); // Attempts to ensure that task t never completes iteself.
+
+            // Assert
+            t.IsCompleted.Should().BeFalse();
+            result.IsCompleted.Should().BeTrue();
+
+            result.Result.Dispose();
+
+            await Task.Delay(1_000); // Attempts to ensure that t will change status after work will finished.
+
+            t.IsCompleted.Should().BeTrue();
+        }
+
+
     }
 }
