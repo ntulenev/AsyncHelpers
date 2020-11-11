b7ac5c43520bac3ad8b8178117bb205561c3131d
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-11 21:31:55 +0300

    Add ValueTaskCompletionSourceInternal raw version

---
+++ src/AsyncHelpers/AsyncHelpers/ValueTaskCompletionSource{T}.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+    // Early version need to fix
+
+    public class ValueTaskCompletionSource<T>
+    {
+        public bool TrySetResult(T result) => _vts.TrySetResult(result, _vts.Version);
+
+        public void SetResult(T result)
+        {
+            if (!TrySetResult(result))
+                throw new InvalidOperationException();
+        }
+
+        public bool TrySetCanceled() => _vts.TrySetCanceled(_vts.Version);
+
+        public void SetCanceled()
+        {
+            if (!TrySetCanceled())
+                throw new InvalidOperationException();
+        }
+
+        public bool TrySetException(Exception ex)
+        {
+            if (ex == null)
+                throw new ArgumentNullException(nameof(ex));
+            return _vts.TrySetException(ex, _vts.Version);
+        }
+
+        public void SetException(Exception ex)
+        {
+            if (ex == null)
+                throw new ArgumentNullException(nameof(ex));
+            if (!TrySetException(ex))
+                throw new InvalidOperationException();
+        }
+
+        public ValueTaskCompletionSource(bool runContinuationsAsynchronously)
+        {
+            _vts = new ValueTaskCompletionSourceInternal<T>(runContinuationsAsynchronously);
+        }
+
+        public ValueTask<T> Task => _vts.Task;
+
+        private ValueTaskCompletionSourceInternal<T> _vts;
+    }
+}
