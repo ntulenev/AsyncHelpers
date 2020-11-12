1e4d0322656aea2e688b2efef284b855b2038c93
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-12 21:09:50 +0300

    Add SinglePhaseAsyncBarrier

---
+++ src/AsyncHelpers/AsyncHelpers/SinglePhaseAsyncBarrier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+
+    //TODO Add Comments
+
+    public class SinglePhaseAsyncBarrier
+    {
+        public SinglePhaseAsyncBarrier(int participantCount)
+        {
+            if (participantCount < 0)
+            {
+                throw new ArgumentException("Value should be positive.", nameof(participantCount));
+            }
+
+            _participantCount = participantCount;
+            _tcsItems = new List<TaskCompletionSource<object>>(participantCount);
+        }
+
+        public Task SignalAndWaitAsync()
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            AddToBarrierAndCheck(tcs);
+
+            return tcs.Task;
+        }
+
+        private void AddToBarrierAndCheck(TaskCompletionSource<object> tcs)
+        {
+            lock (_barrierCheckGuard)
+            {
+                _tcsItems.Add(tcs);
+
+                if (_tcsItems.Count == _participantCount)
+                {
+                    foreach (var item in _tcsItems)
+                    {
+                        item.SetResult(null!);
+                    }
+
+                    _tcsItems.Clear();
+                }
+            }
+        }
+
+        private readonly object _barrierCheckGuard = new object();
+
+        private readonly List<TaskCompletionSource<object>> _tcsItems;
+
+        private readonly int _participantCount;
+    }
+}
