542b167c3d0a5faf650a7c698c42edd6c696e04f
Nikita Tulenev <n1tulenev@gmail.com>
2020-11-13 18:11:25 +0300

    Add WaitAllTaskButCheck metod

---
+++ src/AsyncHelpers/AsyncHelpers/TaskExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncHelpers
+{
+    /// <summary>
+    /// Extensions for tasks
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Waits all tasks but invoke <paramref name="OnFaulted"/> if any task is failed
+        /// </summary>
+        public static async Task WaitAllTaskButCheck(this IEnumerable<Task> tasks, Action OnFaulted)
+        {
+            if (OnFaulted == null)
+                throw new ArgumentNullException(nameof(OnFaulted));
+
+            var any = Task.WhenAny(tasks).Unwrap();
+            _ = any.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    OnFaulted();
+                }
+            });
+            await Task.WhenAll(tasks);
+        }
+    }
+}
