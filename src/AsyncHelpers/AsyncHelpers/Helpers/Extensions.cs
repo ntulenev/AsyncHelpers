d662921865fe38ab4e61923022c00b4b3f6cc356
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-22 23:12:56 +0300

    Add WithCancellation helper

--- src/AsyncHelpers/AsyncHelpers/Helpers/Extensions.cs
+++ src/AsyncHelpers/AsyncHelpers/Helpers/Extensions.cs
@@ -12,15 +12,9 @@
         /// <exception cref="ArgumentNullException">Throws if any arg is null.</exception>
         public static async Task WaitAllTasksButCheckAsync(this IEnumerable<Task> tasks, Action onFaulted)
         {
-            if (tasks == null)
-            {
-                throw new ArgumentNullException(nameof(tasks));
-            }
+            ArgumentNullException.ThrowIfNull(tasks);
 
-            if (onFaulted == null)
-            {
-                throw new ArgumentNullException(nameof(onFaulted));
-            }
+            ArgumentNullException.ThrowIfNull(onFaulted);
 
             var any = Task.WhenAny(tasks).Unwrap();
 
@@ -46,10 +40,7 @@
         /// <exception cref="ArgumentException">Throws if timeout is not positive.</exception>
         public static async Task<bool> TryExecuteWithTimeoutAsync(this Task task, int timeout, CancellationToken ct = default)
         {
-            if (task == null)
-            {
-                throw new ArgumentNullException(nameof(task));
-            }
+            ArgumentNullException.ThrowIfNull(task);
 
             if (timeout <= 0)
             {
@@ -59,6 +50,49 @@
             var resultTask = await Task.WhenAny(task, Task.Delay(timeout, ct)).ConfigureAwait(false);
 
             return resultTask == task;
+        }
+
+        /// <summary>
+        /// Cancels awaiting of <paramref name="task"/> when <paramref name="cancellationToken"/> is canceled.
+        /// </summary>
+        /// <typeparam name="T">Type return type</typeparam>
+        /// <param name="task">Task to cancell.</param>
+        /// <param name="cancellationToken">cancellation token.</param>
+        /// <exception cref="ArgumentNullException">Throws if task is null.</exception>
+        public static Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+
+            if (!cancellationToken.CanBeCanceled)
+            {
+                return task;
+            }
+
+            if (task.IsCompleted)
+            {
+                return task;
+            }
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<T>(cancellationToken);
+            }
+
+            return WithCancellationCoreAsync(task, cancellationToken);
+        }
+
+        private static async Task<T> WithCancellationCoreAsync<T>(Task<T> task, CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource();
+
+            using var _ = ct.Register(() => tcs.SetResult());
+
+            if (task != await Task.WhenAny(task, tcs.Task).ConfigureAwait(false))
+            {
+                ct.ThrowIfCancellationRequested();
+            }
+
+            return await task.ConfigureAwait(false);
         }
     }
 }
