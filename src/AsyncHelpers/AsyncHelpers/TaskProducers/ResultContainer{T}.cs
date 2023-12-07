1edc3fd8a5e7b4d8b2e1569ddb85ead1f06dbb3f
Nikita Tulenev <n1tulenev@gmail.com>
2023-12-07 19:42:34 +0100

    Add primary constuctor for ResultContainer

--- src/AsyncHelpers/AsyncHelpers/TaskProducers/ResultContainer{T}.cs
+++ src/AsyncHelpers/AsyncHelpers/TaskProducers/ResultContainer{T}.cs
@@ -3,24 +3,20 @@
 namespace AsyncHelpers.TaskProducers;
 
 /// <summary>
-/// Container that contains <see cref="RechargeableCompletionSource{T}"/> result. Container should be disposed after result is processed.
+/// Container that contains <see cref="RechargeableCompletionSource{T}"/> result. 
+/// Container should be disposed after result is processed.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class ResultContainer<T> : ActionDispose
+/// <remarks>
+/// Creates container for <see cref="RechargeableCompletionSource{T}"/> result.
+/// </remarks>
+/// <param name="resetAction">logic that should be run on dispose stage.</param>
+/// <param name="value">result.</param>
+/// <exception cref="ArgumentNullException">Thrown when <paramref name="resetAction"/> is null.</exception>
+public class ResultContainer<T>(Action resetAction, T value) : ActionDispose(resetAction)
 {
-    /// <summary>
-    /// Creates container for <see cref="RechargeableCompletionSource{T}"/> result.
-    /// </summary>
-    /// <param name="resetAction">logic that should be run on dispose stage.</param>
-    /// <param name="value">result.</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="resetAction"/> is null.</exception>
-    public ResultContainer(Action resetAction, T value) : base(resetAction)
-    {
-        Value = value;
-    }
-
     /// <summary>
     /// Result value.
     /// </summary>
-    public T Value { get; }
+    public T Value { get; } = value;
 }
